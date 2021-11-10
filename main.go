53b4aa48d1a4226877af1c51b6ec28a7114dcdd2
Ananda Badari <abadari3@gatech.edu>
2021-11-09 21:30:28 -0500

    ananda website

--- main.go
+++ main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	port := "3000"
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+	// if port == "" {
+	// 	log.Fatal("$PORT must be set")
+	// }
 
 	router := gin.New()
 	router.Use(gin.Logger())
